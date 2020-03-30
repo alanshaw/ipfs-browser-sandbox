ac340d230546015dcd0b702f0ea263be962b806f
Alan Shaw <alan.shaw@protocol.ai>
2020-03-30 23:27:24 +0100

    fix: focus search bar on create new tab
    resolves #9

--- src/ui/App.js
+++ src/ui/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Tabs } from './Tabs'
-import { Toolbar } from './Toolbar'
+import Toolbar from './Toolbar'
 import { Page } from './Page'
 
 function App () {
--- src/ui/Toolbar.js
+++ src/ui/Toolbar.js
@@ -1,21 +1,29 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { setTabSearch, setTabUrl, selectSelectedTab } from './redux/slices/tabs'
+import React, { createRef, Component } from 'react'
+import { connect } from 'react-redux'
+import { setTabSearch, setTabUrl, selectSelectedTab, selectTabList } from './redux/slices/tabs'
 import { selectPeersList } from './redux/slices/peers'
 import isIpfs from 'is-ipfs'
 import CID from 'cids'
 import BackButton from './BackButton'
 import ForwardButton from './ForwardButton'
 
-export function Toolbar () {
-  const tab = useSelector(selectSelectedTab)
-  const peers = useSelector(selectPeersList)
-  const dispatch = useDispatch()
+export class Toolbar extends Component {
+  constructor (props) {
+    super(props)
+    this.searchBar = createRef()
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
 
-  if (!tab) return null
+  componentDidUpdate (prevProps) {
+    if (this.props.tab && !prevProps.tabs.some(t => t.id === this.props.tab.id)) {
+      this.searchBar.current.focus()
+    }
+  }
 
-  const onSubmit = e => {
+  handleSubmit (e) {
     e.preventDefault()
+
+    const { tab } = this.props
     let search = tab.search
     let url
 
@@ -54,30 +62,47 @@ export function Toolbar () {
     }
 
     if (search !== tab.search) {
-      dispatch(setTabSearch({ tabId: tab.id, value: search }))
+      this.props.setTabSearch({ tabId: tab.id, value: search })
     }
 
-    dispatch(setTabUrl({ tabId: tab.id, value: url }))
+    this.props.setTabUrl({ tabId: tab.id, value: url })
   }
 
-  return (
-    <form onSubmit={onSubmit} className='pa2 flex bb b--gray items-center'>
-      <span className='mr1'>
-        <BackButton />
-      </span>
-      <span className='mr2'>
-        <ForwardButton />
-      </span>
-      <input
-        className='input-reset bg-white-90 ba b--gray mr2 pa2 br2 db f6 w-100 outline-0'
-        style={{ boxShadow: '2px 2px 4px 0 rgba(0,0,0,.06)' }}
-        value={tab.search || ''}
-        onChange={e => dispatch(setTabSearch({ tabId: tab.id, value: e.target.value }))}
-        placeholder='Search or enter address'
-      />
-      <span className='nowrap mr1'>
-        <span className='v-mid f7 charcoal'>{peers.length} <span className='charcoal-muted'>peers</span></span>
-      </span>
-    </form>
-  )
+  render () {
+    const { tab, peers, setTabSearch } = this.props
+
+    if (!tab) return null
+
+    return (
+      <form onSubmit={this.handleSubmit} className='pa2 flex bb b--gray items-center'>
+        <span className='mr1'>
+          <BackButton />
+        </span>
+        <span className='mr2'>
+          <ForwardButton />
+        </span>
+        <input
+          ref={this.searchBar}
+          className='input-reset bg-white-90 ba b--gray mr2 pa2 br2 db f6 w-100 outline-0'
+          style={{ boxShadow: '2px 2px 4px 0 rgba(0,0,0,.06)' }}
+          value={tab.search || ''}
+          onChange={e => setTabSearch({ tabId: tab.id, value: e.target.value })}
+          placeholder='Search or enter address'
+        />
+        <span className='nowrap mr1'>
+          <span className='v-mid f7 charcoal'>{peers.length} <span className='charcoal-muted'>peers</span></span>
+        </span>
+      </form>
+    )
+  }
 }
+
+const mapStateToProps = state => ({
+  tab: selectSelectedTab(state),
+  tabs: selectTabList(state),
+  peers: selectPeersList(state)
+})
+
+const mapDispatchToProps = { setTabSearch, setTabUrl }
+
+export default connect(mapStateToProps, mapDispatchToProps)(Toolbar)
